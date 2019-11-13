412bce85bb43ec66aa32ec2dc071a16ecbdbce8d
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-11-13 09:38:01 -0500

    fixed find queries and delete query

--- index.js
+++ index.js
@@ -19,39 +19,40 @@ app.post("/shades", function (req, res){
     })
 })
 
-// show Brand
-app.get("/shades/:brand", function(req, res) {
-    Shade.find({ brand: req.params.brand }).then(shades => {
-      res.json(shades);
+//Search by Brand, Product, Hex, or Group
+    // show Brand
+    app.get("/shades/brand/:brand", function(req, res) {
+        Shade.find({ brand: req.params.brand }).then(shades => {
+        res.json(shades);
+        });
     });
-});
 
-// show Product
-app.get("/shades/:product", function(req, res) {
-    Shade.find({ product: req.params.product }).then(shades => {
-      res.json(shades);
+    // show Product
+    app.get("/shades/product/:product", function(req, res) {
+        Shade.find({ product: req.params.product }).then(shades => {
+        res.json(shades);
+        });
     });
-});
 
-// show Hex
-app.get("/shades/:hex", function(req, res) {
-    Shade.find({ hex: req.params.hex }).then(shades => {
-      res.json(shades);
+    // show Hex
+    app.get("/shades/hex/:hex", function(req, res) {
+        Shade.find({ hex: req.params.hex }).then(shades => {
+        res.json(shades);
+        });
     });
-});
 
-// show Group
-app.get("/shades/:group", function(req, res) {
-    Shade.find({ group: req.params.group }).then(shades => {
-      res.json(shades);
+    // show Group
+    app.get("/shades/group/:group", function(req, res) {
+        Shade.find({ group: req.params.group }).then(shades => {
+        res.json(shades);
+        });
     });
-});
 
-// edit one
+// edit one (test this!) Find one by id and change the value of hex
 app.get("/shades/:id/edit", function(req, res) {
     Shade.findOneAndUpdate(
-        {_id: req.params.id},
-        req.body,
+        {_id: req.params.id}, //what it's finding
+        { $set: {hex: req.body.hex}}, //what it's updating to
         {new: true}
     ).then(shades => {
         res.json(shades);
@@ -60,7 +61,7 @@ app.get("/shades/:id/edit", function(req, res) {
 
 //delete one
 app.delete("/shades/:id", function(req, res) {
-    Shade.findOneAndDelete({hex: req.params.hex}).then(shades => {
+    Shade.findOneAndDelete({_id: req.body.id}).then(shades => {
         res.json(shades);
     })
 })
